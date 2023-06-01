1482077e92e0a5bdf6e1f974564a6c5435972123
Your Name <you@example.com>
2023-05-31 21:36:36 -0700

    login

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
 import { BrowserRouter as Router,Link, Route, Routes } from 'react-router-dom';
-
+import React, {useState} from 'react';
+import {Login} from "./components/Login";
+import {Sign_in} from "./components/Signin";
 import Homepage from './components/homepage';
 import LandingPage from './components/landingpage';
 import Tpostcreate from './components/tpostcreate';
 import './App.css';
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
   return (
-    <Router>
     <div className="App">
-      <nav>
-        <ul>
-        <li>
-            <Link to="/">Homepage</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/tpostcreate">Create post</Link>
-          </li>
-        </ul>
-      </nav>
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm}/> : <Sign_in onFormSwitch={toggleForm}/>
+      }
     </div>
-    <Routes>
-    <Route path="/" element={<Homepage />} />
-    <Route path="/login" element={<LandingPage />} />
-    <Route path="/tpostcreate" element={<Tpostcreate />} />
-    </Routes>
     
-    </Router>
   );
 }
 
