1f8d5661ba044f1ae3333f2d4530494898b93016
Your Name <you@example.com>
2023-06-08 17:45:20 -0700

    up date on sigin up

--- frontend/src/components/Signin.js
+++ frontend/src/components/Signin.js
@@ -7,6 +7,9 @@ function Sign_in (props){
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [username, setUsername] = useState('');
+    const [gender, setGender] = useState('');
+    const [Birthdate, setBirthdate] = useState('');
+    const [phone_number , setPhone] =useState('');
     const {signup, error, isLoading} = useSignup()
 
     const handleSubmit = async (e) =>{
@@ -19,7 +22,7 @@ function Sign_in (props){
         <div className="logo-div">
             <img src={logo} alt="logo of the project"/>
         <div className="auth-form-container">
-                <h2>SignIn</h2>
+                <h2>Sign Up</h2>
             <form className="signin-form" onSubmit={handleSubmit}>
                 <label htmlFor="username"> Full username </label>
                     <input value={username} onChange={(e) => setUsername (e.target.value)} username="username" id="username" placeholder="full username"/>
@@ -27,7 +30,18 @@ function Sign_in (props){
                     <input value={email} onChange= {(e) => setEmail (e.target.value)} type="email" username="email"/>
                     <label htmlFor="password">password </label>
                     <input value={password} onChange={(e) => setPassword (e.target.value)} type="password" placeholder="***********" id="password" username="password"/>
-                    <button type="submit" disabled={isLoading}> Sign In</button>
+                    <label htmlFor="birthdate"> birthdate</label>
+                    <input type="date" value={Birthdate} onChange={(e) => setBirthdate (e.target.value)} name="Birthdate" id="birhtdate"/>
+                    <label htmlFor="phone_number">phone number </label>
+                    <input value={phone_number} onChange={(e) => setPhone (e.target.value)} username="phone_number" id="phone_number"/>
+                    <label htmlFor="gender">gender</label>
+                        <select className="Gender" onChange={(e) => setGender(e.target.value)}>
+                            <option value="decline to say">decline to say </option>
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                        </select>
+                        <input type="checkbox" /> <span>Agree with terms and conditions.</span>
+                    <button type="submit" disabled={isLoading}> Sign Up</button>
 
             </form>
             <button className="link-btn" onClick={() => props.onFormSwitch('login')}> Alredy have an account? Login in here. </button>
